cbba0ab81002f0e3ad2d3ac7a612fd664e862371
Sidhaarthsr <srsidhaarth@gmail.com>
2023-09-23 15:51:19 -0400

    sidhaarth : IdGenerator v1
    TODO : Revisit if there are any limitations

---
+++ src/main/java/com/warzone/util/IdGenerator.java
@@ -0,0 +1,19 @@
+package com.warzone.util;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class IdGenerator {
+
+    private static final AtomicLong counter = new AtomicLong(0);
+
+    /**
+     * Private constructor to prevent instantiation
+     */
+    private IdGenerator() {
+
+    }
+
+    public static Long generateId() {
+        return counter.incrementAndGet();
+    }
+}
