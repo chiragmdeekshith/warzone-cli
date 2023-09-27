ce210c0aa6ab47f6eec0524268db133ba2dc4b13
Rowan Kerwin <rowan.kerwin@mail.mcgill.ca>
2023-09-27 13:40:43 -0400

    startup phase functionality (without maps)

--- src/main/java/com/fsociety/warzone/game/engine/startup/StartUp.java
+++ src/main/java/com/fsociety/warzone/game/engine/startup/StartUp.java
@@ -14,30 +14,32 @@ public class StartUp {
 
     public static void start_up() {
         Scanner l_scanner = new Scanner(System.in);
-        System.out.println("Reached startup phase. Enter a startup phase command to go ahead.");
+        System.out.println("New game selected. Please start by loading a map.");
         String l_inputRawCommand = "";
         while(!l_inputRawCommand.equals(StartupCommand.BACK)) {
             System.out.println("Enter command.");
             System.out.print("> ");
             l_inputRawCommand = l_scanner.nextLine();
-            if(CommandHandler.isValidCommand(l_inputRawCommand, Phase.START_UP)){
-                System.out.println("Command is valid!!");
+            if(CommandHandler.isValidCommand(l_inputRawCommand, Phase.START_UP) && l_inputRawCommand.split(" ")[0].equals("loadmap")){
+                loadMap(l_inputRawCommand.split(" ")[1]);
+                l_scanner.close();
+                l_inputRawCommand = editPlayers();
+                break;
             } else {
-                System.out.println("Command is invalid.");
+                System.out.println("Please start by loading a map.");
             }
         }
-
-
-
-        load_map();
-        edit_players();
-        assign_countries();
+        if (l_inputRawCommand.equals(StartupCommand.BACK)) {
+            // go back to main menu
+        } else {
+            assignCountries();
+        }
     }
 
     /**
      * @TODO Implement map loading using FileIO
      */
-    public static void load_map() {
+    public static void loadMap(String p_fileName) {
 
         PlayMap l_map = new PlayMap();
 
@@ -48,17 +50,58 @@ public class StartUp {
     /**
      * @TODO Implement command parser to edit list of players
      */
-    public static void edit_players() {
+    public static String editPlayers() {
 
         ArrayList<Player> l_players = new ArrayList<>();
 
-        GameEngine.set_players(l_players);
+        Scanner l_scanner = new Scanner(System.in);
+        System.out.println("Please create list of players.");
+        String l_inputRawCommand = "";
+        while(!l_inputRawCommand.equals(StartupCommand.BACK)) {
+            System.out.println("Enter command.");
+            System.out.print("> ");
+            l_inputRawCommand = l_scanner.nextLine();
+            if(CommandHandler.isValidCommand(l_inputRawCommand, Phase.START_UP) && l_inputRawCommand.split(" ")[0].equals("gameplayer")){
+                String l_name = l_inputRawCommand.split(" ")[2];
+                if (l_inputRawCommand.split(" ")[0].equals("-add")) {
+                    if (!containsPlayer(l_players, l_name)) {
+                        l_players.add(new Player(l_name));
+                    } else {
+                        System.out.println("Player " + l_name + " already exists.");
+                    }
+
+                }
+            } else if ((CommandHandler.isValidCommand(l_inputRawCommand, Phase.START_UP) && l_inputRawCommand.split(" ")[0].equals("assigncountries"))) {
+                if (l_players.size() == 1) {
+                    System.out.println("Please add at least one player to the game to continue.");
+                } else {
+                    break;
+                }
+            }
+        }
+        if (l_inputRawCommand.equals(StartupCommand.BACK)) {
+            return "back";
+        } else {
+            GameEngine.set_players(l_players);
+            return "assigncountries";
+        }
+    }
+
+    public static boolean containsPlayer(ArrayList<Player> p_players, String p_name) {
+        for (int i = 0; i < p_players.size(); i++) {
+            if (p_players.get(i).getName().equals(p_name)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
      * @TODO Assign countries randomly to each player based on map
      */
-    public static void assign_countries() {
+    public static void assignCountries() {
+        // Assign countries randomly
+        // Then start the game loop
 
     }
 
