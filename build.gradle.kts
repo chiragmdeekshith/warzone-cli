eb74f7db95fc5160e636c42877f8a5c599a69886
chiragmdeekshith <chiragmdeekshith@gmail.com>
2023-11-08 11:36:30 -0500

    update gradle build file for javadoc options

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,21 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("junit:junit:4.13.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<Javadoc> {
+    options {
+        this as StandardJavadocDocletOptions
+        addStringOption("Xdoclint:all")
+        addStringOption("Xwerror", "-quiet")
+    }
 }
 
 sourceSets {
