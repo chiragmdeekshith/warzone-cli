c4e9e35fe387f95d9555a49f60acee1f3cfc0ae3
chiragmdeekshith <chiragmdeekshith@gmail.com>
2023-11-08 14:06:33 -0500

    validator javadoc and tests

--- src/test/java/com/fsociety/warzone/asset/command/CommandValidatorTest.java
+++ src/test/java/com/fsociety/warzone/asset/command/CommandValidatorTest.java
@@ -1,17 +1,62 @@
 package com.fsociety.warzone.asset.command;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The test suite for all tests in the CommandValidator class
+ */
 class CommandValidatorTest {
 
-    @BeforeEach
-    void setUp() {
+    /**
+     * Ensures that unrecognized commands are not valid
+     */
+    @Test
+    public void testValidateCommand_UnrecognizedCommand() {
+        assertFalse(CommandValidator.validateCommand(new String[]{"random"}));
     }
 
-    @AfterEach
-    void tearDown() {
+    /**
+     * Ensures that commands with too few arguments are not valid
+     */
+    @Test
+    public void testValidateCommand_TooFewArguments() {
+        assertFalse(CommandValidator.validateCommand(new String[]{"editmap"}));
+        assertFalse(CommandValidator.validateCommand(new String[]{"editcountry"}));
+        assertFalse(CommandValidator.validateCommand(new String[]{"editneighbor"}));
+        assertFalse(CommandValidator.validateCommand(new String[]{"gameplayer"}));
+        assertFalse(CommandValidator.validateCommand(new String[]{"deploy"}));
+        assertFalse(CommandValidator.validateCommand(new String[]{"advance"}));
+        assertFalse(CommandValidator.validateCommand(new String[]{"bomb"}));
+        assertFalse(CommandValidator.validateCommand(new String[]{"negotiate"}));
     }
+
+    /**
+     * Ensures that commands with invalid arguments types are not valid
+     */
+    @Test
+    public void testValidateCommand_InvalidArgumentType() {
+        assertFalse(CommandValidator.validateCommand(new String[]{"editcountry", "-add", "blah", "blah"}));
+        assertFalse(CommandValidator.validateCommand(new String[]{"editneighbor", "-remove", "blah", "blah"}));
+    }
+
+    /**
+     * Unit test for the common single argument check function
+     */
+    @Test
+    public void testValidateNoArgsCommand_TooManyArguments() {
+        assertFalse(CommandValidator.validateNoArgsCommand(new String[]{"showmap", "extra_argument"}));
+        assertFalse(CommandValidator.validateNoArgsCommand(new String[]{"commit", "extra_argument"}));
+    }
+
+    /**
+     * Unit test for the common filename check function
+     */
+    @Test
+    public void testValidateFilenameCommand_InvalidArguments() {
+        assertFalse(CommandValidator.validateFilenameCommand(new String[]{"savemap"}));
+        assertFalse(CommandValidator.validateFilenameCommand(new String[]{"loadmap", "filename.map", "blah"}));
+    }
+
 }
