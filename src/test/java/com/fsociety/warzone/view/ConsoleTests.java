01a780c3778b69ab842647ecdcd85e174ca50cad
Sidhaarthsr <srsidhaarth@gmail.com>
2023-11-28 21:08:54 -0500

    dev/sidhaarth : Reverting some tests

--- src/test/java/com/fsociety/warzone/view/ConsoleTests.java
+++ src/test/java/com/fsociety/warzone/view/ConsoleTests.java
@@ -13,30 +13,16 @@ class ConsoleTests {
 
     private final ByteArrayOutputStream d_outContent = new ByteArrayOutputStream();
     private final PrintStream d_originalOut = System.out;
-    private final InputStream originalIn = System.in;
-    private ByteArrayInputStream d_inStream;
     private final String d_file = "src/test/resources/logs/console_test.log";
 
     @BeforeEach
     public void setUpStreams() {
-        String l_input = "This is my input";
-        d_inStream = new ByteArrayInputStream(l_input.getBytes());
-        System.setIn(d_inStream);
         System.setOut(new PrintStream(d_outContent));
     }
 
     @AfterEach
     public void restoreStreams() {
-        System.setIn(originalIn);
         System.setOut(d_originalOut);
-    }
-
-    @Test
-    void testCommandPromptPlayer() {
-        final String l_playerName = "player1";
-        final String l_prompt = "Please enter a command.\n" + l_playerName + "> ";
-        Console.commandPromptPlayer(l_playerName);
-        Assertions.assertEquals(l_prompt, d_outContent.toString());
     }
 
     @Test
